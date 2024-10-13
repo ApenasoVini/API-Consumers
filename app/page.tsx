901fb936aae9d4cf2b5da0895b8909fbda32b1c6
vini <viniciusicmsc@gmail.com>
2024-10-12 21:16:22 -0300

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { useState } from "react";
 import Image from "next/image";
+import { useState } from "react";
 import Card from "./components/card/card";
 
 export default function Home() {
@@ -42,39 +42,27 @@ export default function Home() {
   return (
     <div className="bg-gray-50 min-h-screen w-full">
       <div className="relative h-screen overflow-hidden">
-        <header className="bg-gradient-to-r z-20 from-gray-700 via-gray-900 to-black text-white shadow-2xl">
-          <nav className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-5 flex justify-between items-center">
-            <h1 className="text-3xl font-extrabold tracking-wider">API Hub</h1>
-            <ul className="flex space-x-8 text-lg">
-              <li>
-                <a href="#features" className="hover:text-indigo-400 transition duration-300">
-                  Products
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </header>
         {slides.map((slide, index) => (
           <div
             key={index}
             className={`absolute inset-0 flex justify-center items-center transition-opacity duration-1000 ${index === currentSlide ? "opacity-100" : "opacity-0"}`}
+            style={{
+              backgroundImage: `url(${slide.image})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              backgroundAttachment: 'fixed',
+            }}
           >
-            <Image
-              src={slide.image}
-              alt={`Slide ${index + 1}`}
-              layout="fill"
-              objectFit="cover"
-            />
-            <div className="z-10 flex flex-col items-center">
+            <div className="z-10 flex flex-col items-center bg-black bg-opacity-50 p-6 rounded-lg">
               <h2 className="text-5xl font-bold text-white drop-shadow-lg sm:text-6xl">
                 {slide.title}
               </h2>
-              <p className="mt-4 text-lg bg-gradient-to-r text-gray-100">{slide.description}</p>
+              <p className="mt-4 text-lg text-white">{slide.description}</p>
               <a
                 href="#features"
                 className="mt-8 inline-block bg-indigo-600 text-white px-6 py-3 rounded-full shadow-lg hover:bg-indigo-700 transition-transform transform hover:scale-105"
               >
-                Learn More
+                Ver APIs
               </a>
             </div>
           </div>
@@ -109,12 +97,12 @@ export default function Home() {
         </div>
       </section>
 
-      <footer className="bg-gray-900 text-white py-16">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="text-center mb-12">
-            <h3 className="text-3xl font-semibold text-gray-200">About the Project</h3>
+      <footer className="bg-gray-900 text-white px-4 py-20">
+        <div className="flex justify-between items-center">
+          <div className="text-center w-60">
+            <h3 className="text-3xl font-semibold text-gray-200">Sobre o projeto</h3>
             <p className="mt-4 text-lg text-gray-400">
-              This project was developed to provide users with easy access to multiple APIs, enhancing productivity and integrating external services.
+              Esse projeto foi desenvolvido para a vizualização do uso de APIs públicas, estimulando seu uso e o aprendizado em geral.
             </p>
           </div>
 
